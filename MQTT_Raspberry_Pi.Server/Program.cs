2d3d90537d964642c359b45fbed56e7e3245ce5e
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-11-05 20:34:07 +0330

    Add project files.

---
+++ MQTT_Raspberry_Pi.Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using MQTTnet.Server;
+using System;
+using MQTTnet;
+using System.Text;
+using static System.Console;
+using System.Threading;
+using static SharedItems.LogMessages;
+
+var wait = new ManualResetEvent(false);
+var option = new MqttServerOptionsBuilder().WithDefaultEndpoint()
+    .WithDefaultEndpointPort(1883)
+    .WithConnectionValidator(OnNewConnection)
+    .WithApplicationMessageInterceptor(OnNewMessage);
+var mqttServer = new MqttFactory().CreateMqttServer();
+await mqttServer.StartAsync(option.Build());
+ReadLine();
+wait.WaitOne();
